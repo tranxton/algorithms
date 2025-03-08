c82d2f836b54da654f1f9bb281324495942563b6
Rustem Akhmetzianov <rustem.akhmetzianov@gmail.com>
2025-03-08 23:10:52 +0300

    Applied standard project directories structure

---
+++ cmd/go_basics/go_basics.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/tranxton/algorithms/internal/go_basics"
+)
+
+func main() {
+	testingGoBasics()
+}
+
+func testingGoBasics() {
+	go_basics.HelloWorld()
+	go_basics.FizzBuzz()
+	go_basics.TestingErrorProcessing()
+	go_basics.DoPanic()
+	go_basics.DeferTest()
+	go_basics.TestingInteger()
+	go_basics.TestingFloatingPointNumbers()
+	go_basics.TestingBoolean()
+	go_basics.TestingString()
+	go_basics.TestingBackticks()
+	go_basics.TestingUserTypes()
+	go_basics.TestingAliases()
+	go_basics.TestingAlternativeWaysOfVariablesDeclaration()
+	go_basics.TestingUntypedConstants()
+	go_basics.TestingTypedConstants()
+	go_basics.TestingEnums()
+	go_basics.UserTypesInConstants()
+	go_basics.TestingLiterals()
+	go_basics.TestingIota()
+	go_basics.TestingGlobalScope()
+	go_basics.TestingExport()
+	go_basics.TestingConditionalOperators()
+	go_basics.TestingCycles()
+	go_basics.Pointers()
+	go_basics.LiteralPointer()
+	go_basics.StructPointer()
+	go_basics.UserInputCounter()
+	go_basics.Arrays()
+	go_basics.MultidimensionalArray()
+	go_basics.Slices()
+	go_basics.ChangingSliceSize()
+	go_basics.WorkingWithSlicesInFunc()
+	go_basics.ChangingSliceSizeInFunc()
+	go_basics.OperationsOnSlices()
+	go_basics.TestTaskWithSlices()
+}
---
+++ cmd/leet_code/leet_code.go
@@ -0,0 +1,125 @@
+package main
+
+func main() {
+	passingLeetCodeProblems()
+}
+
+func passingLeetCodeProblems() {
+	/*tree := leet_code.TreeNode{
+		Val: 1,
+		Left: &leet_code.TreeNode{
+			Val: 2,
+			Left: &leet_code.TreeNode{
+				Val: 4,
+				Left: &leet_code.TreeNode{
+					Val: 8,
+				},
+			},
+			Right: &leet_code.TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &leet_code.TreeNode{
+			Val: 3,
+			Left: &leet_code.TreeNode{
+				Val: 6,
+			},
+			Right: &leet_code.TreeNode{
+				Val: 7,
+			},
+		},
+	}
+
+	fmt.Println(leet_code.CountNodes(&tree))*/
+
+	/*fmt.Println(leet_code.MissingNumber([]int{1, 2, 4, 5, 6}))
+	fmt.Println(leet_code.MySqrt(8))
+	fmt.Println(leet_code.Intersection([]int{8, 0, 3}, []int{0, 0}))
+	fmt.Println(leet_code.IsPerfectSquare(1664))
+	fmt.Println(leet_code.Intersect([]int{2}, []int{4}))
+	fmt.Println(leet_code.GuessNumber(10))
+	fmt.Println(leet_code.ContainsDuplicate([]int{0, 1, 1}))
+	fmt.Println(leet_code.IsValid("}{"))
+	fmt.Println(leet_code.IsPalindromeStack(9))*/
+
+	/*nums2 := []int{2, 5, 6}
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	leet_code.Merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)*/
+
+	/*pTree := leet_code.TreeNode{
+		Val: 1,
+		Left: &leet_code.TreeNode{
+			Val: 2,
+		},
+		Right: &leet_code.TreeNode{
+			Val: 3,
+		},
+	}
+
+	qTree := leet_code.TreeNode{
+		Val: 1,
+		Left: &leet_code.TreeNode{
+			Val: 2,
+		},
+		Right: &leet_code.TreeNode{
+			Val: 3,
+		},
+	}
+
+	fmt.Println(leet_code.IsSameTree(&pTree, &qTree))*/
+	/**
+		    	   1
+		    	 2 | 2
+		    3 null | null 3
+	 4 null null 4 |
+	*/
+
+	//[1,2,2,3,null,null,3,4,null,null,4]
+	/*tree := leet_code.TreeNode{
+		Val: 1,
+		Left: &leet_code.TreeNode{
+			Val: 2,
+			Left: &leet_code.TreeNode{
+				Val: 4,
+			},
+			Right: &leet_code.TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &leet_code.TreeNode{
+			Val: 3,
+			Right: &leet_code.TreeNode{
+				Val: 3,
+				Right: &leet_code.TreeNode{
+					Val: 4,
+				},
+			},
+			Left: &leet_code.TreeNode{
+				Val: 3,
+			},
+		},
+	}*/
+
+	/*fmt.Println(leet_code.IsSymmetricBfs(&tree))
+	fmt.Println(leet_code.IsSymmetricDfs(&tree))*/
+
+	/*fmt.Println(leet_code.MaxDepthDfs(&tree))
+	fmt.Println(leet_code.MaxDepthBfs(&tree))*/
+
+	//fmt.Println(leet_code.IsBalanced(&tree))
+
+	/*fmt.Println(leet_code.MinDepthDfs(&tree))
+	fmt.Println(leet_code.MinDepthBfs(&tree))*/
+
+	/*fmt.Println(leet_code.HasPathSumDfs(&tree, 10))
+	fmt.Println(leet_code.HasPathSumBfs(&tree, 10))*/
+
+	/*fmt.Println(leet_code.InvertTreeDfs(&tree))
+	fmt.Println(leet_code.InvertTreeBfs(&tree))*/
+
+	/*fmt.Println(leet_code.SumOfLeftLeavesDfs(&tree))
+	fmt.Println(leet_code.SumOfLeftLeavesBfs(&tree))*/
+
+	//fmt.Println(leet_code.Search([]int{4, 5, 6, 7, 0, 1, 2}, 2))
+}

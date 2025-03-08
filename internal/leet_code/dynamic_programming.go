06c047448252def3b583485134ab715af41451ef
Rustem Akhmetzianov <rustem.akhmetzianov@gmail.com>
2025-03-08 11:13:04 +0300

    Solved problem

--- internal/leet_code/dynamic_programming.go
+++ internal/leet_code/dynamic_programming.go
@@ -23,3 +23,62 @@ func MaxProfit(prices []int) int {
 
 	return 0
 }
+
+func Fibonacci(n int) uint64 {
+	var memory = make([]uint64, n+1)
+
+	var fibonacci func(int) uint64
+	fibonacci = func(n int) uint64 {
+		if n < 2 {
+			return uint64(n)
+		}
+
+		if memory[n] != 0 {
+			return memory[n]
+		}
+
+		result := fibonacci(n-1) + fibonacci(n-2)
+		memory[n] = result
+
+		return result
+	}
+
+	return fibonacci(n)
+}
+
+func FindLongestCommonSubstring(target string, words []string) string {
+	if len(words) == 0 {
+		return ""
+	}
+
+	maxLongestCommonSubstring, maxWordIndex := 0, -1
+
+	for i, word := range words {
+		wordMaxLongestCommonSubstring := 0
+		for targetI := 0; targetI < len(target); targetI++ {
+			for wordI := 0; wordI < len(word); wordI++ {
+				currentLongestCommonSubstring := 0
+				for targetI+currentLongestCommonSubstring < len(target) &&
+					wordI+currentLongestCommonSubstring < len(word) &&
+					target[targetI+currentLongestCommonSubstring] == word[wordI+currentLongestCommonSubstring] {
+					currentLongestCommonSubstring++
+
+					if currentLongestCommonSubstring > wordMaxLongestCommonSubstring {
+						wordMaxLongestCommonSubstring = currentLongestCommonSubstring
+					}
+				}
+			}
+		}
+
+		if wordMaxLongestCommonSubstring > maxLongestCommonSubstring {
+			maxLongestCommonSubstring = wordMaxLongestCommonSubstring
+			maxWordIndex = i
+		}
+	}
+
+	if maxWordIndex == -1 {
+		return ""
+	}
+
+	return words[maxWordIndex]
+}
--- internal/leet_code/dynamic_programming_test.go
+++ internal/leet_code/dynamic_programming_test.go
@@ -2,6 +2,7 @@ package leet_code
 
 import (
 	"testing"
+	"time"
 )
 
 func TestMaxProfit(t *testing.T) {
@@ -10,5 +11,33 @@ func TestMaxProfit(t *testing.T) {
 
 	if maxProfit != expectedMaxProfit {
 		t.Errorf("Expected max profit to be %d, got %d", expectedMaxProfit, maxProfit)
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	var expectedResult uint64 = 17323038258947941269
+	expectedDurationLimit := 25 * time.Microsecond
+
+	timeStart := time.Now()
+	result := Fibonacci(200)
+	duration := time.Since(timeStart)
+
+	if duration > expectedDurationLimit {
+		t.Errorf("Duration took too long: %v, expected less than %v", duration, expectedDurationLimit)
+	}
+
+	if result != expectedResult {
+		t.Errorf("Expected result to be %d, got %d", expectedResult, result)
+	}
+}
+
+func TestFindLongestCommonSubstring(t *testing.T) {
+	target := "common"
+	words := []string{"test", "comrade", "comma", "comment", "commode", "precommon"}
+	expectedResult := "precommon"
+	result := FindLongestCommonSubstring(target, words)
+
+	if result != expectedResult {
+		t.Errorf("Expected result to be %s, got %s", expectedResult, result)
 	}
 }
